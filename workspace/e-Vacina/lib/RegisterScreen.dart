950ea41157a212a03221f90cd639e25d9e2670a0
GRVial <guivial2010@gmail.com>
2021-03-23 16:31:19 -0300

    refact(#90): changed gender picker on register screen
    Co-authored-by: Erick <erickmelovidal@gmail.com>

--- workspace/e-Vacina/lib/RegisterScreen.dart
+++ workspace/e-Vacina/lib/RegisterScreen.dart
@@ -9,39 +9,74 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class RegisterScreenState extends State<RegisterScreen> {
+  String dropdownValue;
   @override
   Widget build(BuildContext context) {
-    return _menuRegistro(context);
-  }
-}
-
-Widget _menuRegistro(context) {
-  return Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
+    return Scaffold(
       backgroundColor: Colors.white,
-      elevation: 0,
-      centerTitle: true,
-      title: Text(
-        'e-Vacina',
-        style:
-            TextStyle(color: Color.fromRGBO(42, 174, 198, 1.0), fontSize: 25),
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          title: Text(
+            'e-Vacina',
+            style: TextStyle(
+                color: Color.fromRGBO(42, 174, 198, 1.0), fontSize: 25),
+          ),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    print('voltar');
+                  });
+            },
+          )),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MyWidgets().caixaTexto('Nome'),
+            MyWidgets().caixaTexto('CPF', maxLength: 11),
+            MyWidgets().caixaTexto('Data de Nascimento'),
+            genderPick(),
+            MyWidgets().caixaTexto('Email'),
+            MyWidgets().caixaTexto('Senha', isObscure: true),
+            MyWidgets().button('Registrar', 300.0, 50.0),
+          ],
+        ),
       ),
-    ),
-    body: SingleChildScrollView(
-      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          MyWidgets().caixaTexto('Nome'),
-          MyWidgets().caixaTexto('CPF', maxLength: 11),
-          MyWidgets().caixaTexto('Data de Nascimento'),
-          MyWidgets().caixaTexto('Sexo'),
-          MyWidgets().caixaTexto('Email'),
-          MyWidgets().caixaTexto('Senha', isObscure: true),
-          MyWidgets().button('Registrar', 300.0, 50.0),
-        ],
-      ),
-    ),
-  );
+    );
+  }
+
+  Widget genderPick() {
+    return Container(
+      padding: EdgeInsets.only(bottom: 31.5),
+      child: DropdownButtonFormField<String>(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+          hint: Text('Sexo'),
+          value: dropdownValue,
+          isExpanded: true,
+          onChanged: (String newValue) {
+            setState(() {
+              dropdownValue = newValue;
+              print(newValue);
+            });
+          },
+          items: [
+            DropdownMenuItem(
+              child: Text('Masculino'),
+              value: '1', //value pode ser mudado para valor mais significativo
+            ),
+            DropdownMenuItem(
+              child: Text('Feminino'),
+              value: '2',
+            ),
+          ]),
+    );
+  }
 }
