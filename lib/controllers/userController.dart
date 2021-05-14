306786781d42f5b2a579027e60f44e22bf3b22f3
ciro-c <ciro.fga@gmail.com>
2021-05-14 13:13:01 -0300

    style(*): Linted userController

--- lib/controllers/userController.dart
+++ lib/controllers/userController.dart
@@ -1,13 +1,13 @@
 import 'package:mobx/mobx.dart';
 import 'package:dio/dio.dart';
 import 'package:e_vacina/globals.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 part 'userController.g.dart';
 
 class UserController = UserControllerBase with _$UserController;
 
-final _storage = new FlutterSecureStorage();
+// final _storage = new FlutterSecureStorage();
 
 abstract class UserControllerBase with Store {
   @observable
